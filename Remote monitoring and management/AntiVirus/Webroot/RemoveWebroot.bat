32adbceae980ffd9dcee45ea7079a9d7ecfc8de1
John Harrison <Johnharrison712@outlook.com>
2023-05-02 17:04:27 -0500

    Delete Remote monitoring and management directory

--- Remote monitoring and management/AntiVirus/Webroot/RemoveWebroot.bat
+++
@@ -1,2 +0,0 @@
-takeown /F "c:\program files (x86)\webroot" /R /D Y
-"C:\Program Files (x86)\Webroot\WRSA.exe" -uninstall /s /q
--- Remote monitoring and management/NinjaOne/NinjaOne_RMM_Uninstaller.bat
+++
@@ -1,4 +0,0 @@
-wmic service 'NinjaRMMAgent' get pathname | find "C:\" > C:\temp\NinjaRMMuninstaller.txt
-SET /p NinjaFolder= < C:\temp\NinjaRMMuninstaller.txt
-SET Uninstaller=%NinjaFolder:NinjaRMMAgentPatcher.exe"=uninstall.exe" --mode unattended%
-%Uninstaller%
