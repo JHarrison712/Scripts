cb9c193fa675c283b0d6a40ce5c7783205e5cf08
John Harrison <Johnharrison712@outlook.com>
2024-02-12 17:17:19 -0600

    Update Basic_NagiosCore_Install.sh

--- Linux/NagiosCore/Basic_NagiosCore_Install.sh
+++ Linux/NagiosCore/Basic_NagiosCore_Install.sh
@@ -1,1 +1,10 @@
+# Author: John Harrison
+# Creation Date: 2/12/2024
+# Last Modified Date:
+# File Name:Basic_NagiosCore_Install.sh
+# Description: Basic installation of NagiosCore installation script
 
+
+# NagiosCore Install script
+# NOTE: Script wil update and upgrade currently installed packages.
+#!/bin/bash
